c4e5d1ac18279861b69ac8b232a6b36a5c9fc706
SyndaKai <SpencasaurusRex@gmail.com>
2021-12-02 00:14:20 -0500

    2021-Day02-C#

---
+++ 2021/C#/day02/Program.cs
@@ -0,0 +1,50 @@
+﻿string inputFile = @"../../inputs/02.txt";
+
+var input = File.ReadAllText(inputFile);
+var lines = File.ReadAllLines(inputFile);
+
+// Part 1
+int x = 0;
+int y = 0;
+
+foreach (var line in lines) {
+    string[] words = line.Split(" ");
+    string command = words[0];
+    int amount = Int32.Parse(words[1]);
+
+    if (command == "forward") {
+        x += amount;
+    }
+    else if (command == "down") {
+        y += amount;
+    }
+    else if (command == "up") {
+        y -= amount;
+    }
+}
+
+Console.WriteLine(x * y);
+
+// Part 2
+x = 0;
+y = 0;
+int aim = 0;
+
+foreach (var line in lines) {
+    string[] words = line.Split(" ");
+    string command = words[0];
+    int amount = Int32.Parse(words[1]);
+
+    if (command == "forward") {
+        x += amount;
+        y += aim * amount;
+    }
+    else if (command == "down") {
+        aim += amount;
+    }
+    else if (command == "up") {
+        aim -= amount;
+    }
+}
+
+Console.WriteLine(x * y);
