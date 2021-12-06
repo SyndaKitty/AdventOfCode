b2f90ea14487735938063fcc20051b83dd84f235
SyndaKai <SpencasaurusRex@gmail.com>
2021-12-06 00:57:32 -0500

    2021-Day06-C#

---
+++ 2021/C#/day06/Program.cs
@@ -0,0 +1,25 @@
+﻿string inputFile = @"../../inputs/06.txt";
+var input = File.ReadAllText(inputFile);
+var lines = File.ReadAllLines(inputFile);
+
+var numbers = lines[0].Split(",").Select(x => Convert.ToInt64(x));
+
+var fish = new long[9];
+foreach (var num in numbers) {
+    fish[num]++;
+}
+
+for (int i = 0; i < 256; i++) {
+    if (i == 80) {
+        Console.WriteLine(fish.Sum());
+    }
+    long count = fish[0];
+    
+    for (int j = 0; j < 8; j++) {
+        fish[j] = fish[j + 1];
+    }  
+
+    fish[8] = count;
+    fish[6] += count;
+}
+Console.WriteLine(fish.Sum());
