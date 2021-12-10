6a2af1c4be5528add9d50f42994834b2f99fdf23
SyndaKai <SpencasaurusRex@gmail.com>
2021-12-10 06:05:22 -0500

    2021-Day10-C#

---
+++ 2021/C#/day10/Program.cs
@@ -0,0 +1,65 @@
+﻿string inputFile = @"../../inputs/10.txt";
+
+var lines = File.ReadAllLines(inputFile);
+
+Dictionary<char, char> Closing = new Dictionary<char, char> {
+    {'(', ')'},
+    {'[', ']'},
+    {'{', '}'},
+    {'<', '>'},
+};
+Dictionary<char, int> Score = new Dictionary<char, int> {
+    {')', 3},
+    {']', 57},
+    {'}', 1197},
+    {'>', 25137},
+};
+
+// Part 1
+long score = 0;
+foreach (var line in lines) {
+    string buffer = "";
+    foreach (var c in line) {
+        if ("([{<".Contains(c)) {
+            buffer += c;
+        }
+        else if (Closing[buffer.Last()] == c) {
+            buffer = buffer.Substring(0, buffer.Length - 1);
+        }
+        else {
+            score += Score[c];
+            break;
+        }
+    }
+}
+
+Console.WriteLine(score);
+
+// Part 2
+List<long> scores = new List<long>();
+foreach (var line in lines) {
+    score = 0;
+    string buffer = "";
+    bool corrupted = false;
+    foreach (var c in line) {
+        if ("([{<".Contains(c)) {
+            buffer += c;
+        }
+        else if (Closing[buffer.Last()] == c) {
+            buffer = buffer.Substring(0, buffer.Length - 1);
+        }
+        else {
+            corrupted = true;
+            break;
+        }
+    }
+    if (!corrupted) {
+        foreach (var c in buffer.Reverse()) {
+            score *= 5;
+            score += ")]}>".IndexOf(Closing[c]) + 1;
+        }
+        scores.Add(score);
+    }
+}
+scores.Sort();
+Console.WriteLine(scores[scores.Count / 2]);
