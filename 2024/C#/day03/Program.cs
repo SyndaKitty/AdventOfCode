39ab7c5ead8dc6884ed3150f9bd69173dc391b7e
SyndaKai <SyndaKitty@gmail.com>
2024-12-03 00:20:10 -0500

    2024 C# Day03

--- 2024/C#/day03/Program.cs
+++ 2024/C#/day03/Program.cs
@@ -1,17 +1,65 @@
 ﻿var filePath = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..\\..\\..\\..\\..\\inputs\\03.txt"));
-var lines = File.ReadAllLines(filePath);
 var text = File.ReadAllText(filePath);
-var lineTokens = lines.Select(x => x.Split(' ')).ToList();
 
-foreach (var tokens in lineTokens)
+for (int part = 1; part <= 2; part++)
 {
-    foreach (var token in tokens)
+    int sum = 0;
+    bool doMul = true;
+    
+    for (int i = 0; i < text.Length; i++)
     {
-       
+        if (SafeSubstring(text, i, 4) == "mul(" && doMul)
+        {
+            int? a = GetNextNum(text, i + 4, true);
+            int? b = GetNextNum(text, i + 4 + a.ToString().Length + 1, false);
+
+            if (a.HasValue && b.HasValue)
+            {
+                sum += a.Value * b.Value;
+            }
+        }
+        else if (part == 2)
+        {
+            if (SafeSubstring(text, i, 7) == "don't()")
+            {
+                doMul = false;
+            }
+            else if (SafeSubstring(text, i, 4) == "do()")
+            {
+                doMul = true;
+            }
+        }
     }
+    Console.WriteLine(sum);
 }
 
-foreach (var line in lines)
+string SafeSubstring(string text, int index, int length)
 {
-    
+    if (index + length > text.Length)
+    {
+        return text.Substring(index);
+    }
+    return text.Substring(index, length);
+}
+
+int? GetNextNum(string text, int index, bool first)
+{
+    var num = "";
+    for (int i = index; i < text.Length; i++)
+    {
+        if (char.IsDigit(text[i]))
+        {
+            num += text[i];
+        }
+        else if (first && text[i] == ',')
+        {
+            break;
+        }
+        else if (!first && text[i] == ')')
+        {
+            break;
+        }
+        else return null;
+    }
+    return int.Parse(num);
 }
---
+++ ReadmeGen/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+var text = File.ReadAllText("Tally.txt").Replace("\r", "");
+var years = text.Split("\n\n");
+
+StringBuilder readme = new();
+readme.AppendLine("# AdventOfCode");
+readme.AppendLine("My solutions to the Advent of Code challenges.");
+readme.AppendLine();
+
+foreach (var year in years)
+{
+    ParseYear(year, readme);
+}
+
+
+void ParseYear(string yearText, StringBuilder readme)
+{
+    var lines = yearText.Split("\n");
+    var yearNumber = lines[0];
+    readme.AppendLine($"## {yearNumber}");
+
+    var extraLines = (lines.Count() - 1) % 3;
+    Debug.Assert(extraLines == 0, $"Invalid number of lines under year {yearNumber}");
+
+    for (int i = 0; ; i++)
+    {
+        var langLines = lines.Skip(1 + i * 3).Take(3).ToList();
+        if (langLines.Count == 0)
+        {
+            break;
+        }
+        ParseLang(langLines, readme);
+    }
+}
+
+void ParseLang(List<string> lines, StringBuilder readme)
+{
+    string langName = lines[0];
+    string firstStar = lines[1];
+    string secondStar = lines[2];
+
+    readme.AppendLine($"### {langName}");
+
+    ParseStars(firstStar);
+}
+
+void ParseStars()
+{
+
+}
